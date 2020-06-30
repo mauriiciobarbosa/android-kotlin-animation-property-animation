ad41ec59af9bdbebdba52e05ce172e72dac0fc2f
mauricio.santana <mauricio.santana@easynvest.com.br>
2020-06-30 20:20:30 -0300

    adding scale animation.

--- begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -19,6 +19,7 @@ package com.google.samples.propertyanimation
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -88,6 +89,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        ObjectAnimator.ofPropertyValuesHolder(star, scaleX, scaleY).apply {
+            repeatCount = 1
+            repeatMode = ObjectAnimator.REVERSE
+            disableViewDuringAnimation(scaleButton)
+        }.start()
     }
 
     private fun fader() {
